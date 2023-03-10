b8c3dfcb66e0606b9cbe6774ab18378a9f0c86db
chidienwereji <chidienwereji@safeguardglobal.com>
2023-03-10 03:45:10 +0100

    add starting page

---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import { Box, Center } from '@chakra-ui/react';
+import React from 'react';
+
+export default function Main() {
+  return (
+    <Box>
+      <Center bg="tomato" h="100px" color="white">
+        This is the Center
+      </Center>
+    </Box>
+  );
+}
