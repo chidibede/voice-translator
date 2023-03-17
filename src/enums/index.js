312b806a411cdd3badfa0f30e1186b31f5bf660b
chidienwereji <chidienwereji@safeguardglobal.com>
2023-03-17 16:48:05 +0100

    log error

--- src/enums/index.js
+++ src/enums/index.js
@@ -1,5 +1,5 @@
 export const OPEN_AI_KEY=process.env.REACT_APP_OPEN_AI_KEY
 export const OPEN_AI_URL=process.env.REACT_APP_OPEN_AI_URL
-export const OPEN_AI_WHISPER_MODEL="whisper-11"
+export const OPEN_AI_WHISPER_MODEL="whisper-1"
 export const SUPABASE_PROJECT_URL=process.env.REACT_APP_SUPABASE_PROJECT_URL
 export const ANON_KEY=process.env.REACT_APP_ANON_KEY
