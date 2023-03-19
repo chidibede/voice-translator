6e7519ad40419a9f7c46ad1fc58e1539d41b18fc
chidienwereji <chidienwereji@safeguardglobal.com>
2023-03-19 01:06:48 +0100

    aws config outside function

--- src/server/aws/convertTextToSpeech.js
+++ src/server/aws/convertTextToSpeech.js
@@ -2,25 +2,25 @@ import { AWS_ACCESS_KEY, AWS_ACCESS_SECRET_KEY } from '../../enums';
 
 import AWS from 'aws-sdk';
 
+AWS.config.region = 'eu-west-1';
+AWS.config.credentials = {
+  accessKeyId: AWS_ACCESS_KEY,
+  secretAccessKey: AWS_ACCESS_SECRET_KEY,
+};
+
 export const awsConvertTextToSpeech = async (text) => {
-  AWS.config.region = 'eu-west-1';
-  AWS.config.credentials = {
-    accessKeyId: AWS_ACCESS_KEY,
-    secretAccessKey: AWS_ACCESS_SECRET_KEY,
-  };
+  const polly = new AWS.Polly({ apiVersion: '2016-06-10' });
 
-  var polly = new AWS.Polly({ apiVersion: '2016-06-10' });
-
-  var params = {
+  const params = {
     OutputFormat: 'mp3' /* required */,
     Text: text /* required */,
     VoiceId: 'Sergio' /* required */,
     SampleRate: '16000',
     LanguageCode: 'es-ES',
     TextType: 'text',
-    Engine: 'neural'
+    Engine: 'neural',
   };
 
-  const response = await polly.synthesizeSpeech(params).promise()
-  return response
+  const response = await polly.synthesizeSpeech(params).promise();
+  return response;
 };
